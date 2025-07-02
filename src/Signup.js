a9f3a0fd580bbd5d8f2b4274dda554e0df840a17
Abhay Raj <153433014+trevor-09@users.noreply.github.com>
2025-07-02 22:52:44 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -14,7 +14,7 @@ function Signup() {
     setAuthError("");
     setSuccess(false);
     const response = await fetch(
-      "https://todobackend-bi77.onrender.com/register",
+      "https://todobackend-kqc1.onrender.com/register",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
